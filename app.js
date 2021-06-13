5dc5037e90bfc10ebe6fb7c6676427f3282fb47e
Justicexx0099 <mjustin9709@gmail.com>
2021-06-13 17:38:48 +0900

    [Server]/#4/fix/bug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//TODO: 환경설정 필요 
+//TODO: Cookie 환경설정 필요 
 app.use(
   session({
   secret: '@collecting',
@@ -28,8 +28,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin: true,
   credentials: true,
-  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE', 'OPTIONS'],
-  preflightContinue: true,
+  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE', 'OPTIONS']
 }
 app.use(cors(corsOptions));
 
--- controller/signController.js
+++ controller/signController.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
@@ -30,7 +31,9 @@ module.exports = {
 //*회원가입컨트롤러
   signUpController: async(req, res) => {
     //! TODO: client에서 req.body에 socialId 넣었는 지 확인(API 문서 수정)
-    const { isSocialAccount, password, email, username, socialId } = req.body;
+    const { isSocialAccount, password, email, username } = req.body;
+    const { socialId } = req.body.socialId;
+    console.log(req.body.socialId.socialId);
     if(isSocialAccount === 1) {
       await Users.findOrCreate({
         where: { username },
@@ -76,80 +79,78 @@ module.exports = {
 
 //*로그인컨트롤러
   logInController: async(req, res) => {
-    console.log(req.body);
+    console.log(req);
     const { email, password, uuid } = req.body;
+    const { sessionID } = req;
     // 회원가입하고 로그인 할 때는 uuid로 전송 
-    if( uuid === undefined ) {
-      try {
-        const result =  await Users.findOne({
-          where: { email, password }
-        });
-        //!
-        console.log(result);
-        if(!result) {
-          res.status(401).send('Login Failed');
-        } else {
-          delete result.dataValues.password;
-          const accessToken = generateAccessToken(result.dataValues)
-          const refreshToken = generateRefreshToken(result.dataValues);
-          //! 
-          console.log(accessToken);
-          
-          req.session.save( () => {
-            //! session.userId에 uuid 활용
-            req.session.userId = result.dataValues.uuid;
-            res.cookie('refreshToken', refreshToken);
-            res.status(200).send({
-              data: {
-                accessToken,
-                username: result.dataValues.username
-              },
-              message: 'Login Successfully'
-            });
+    // sessionID 
+    if(!sessionID) {
+      //*로그인이 되지 않은 경우
+      if( uuid === undefined ) {
+        //*비소셜 로그인
+          const result =  await Users.findOne({
+            where: { email, password }
           });
-        }
-      } 
-      catch(err) {
-        console.error(err);
-        res.status(422).send('Insufficient Information');
-      }
-    } else {
-      Users.findOne({
-        where: { uuid }
+          if(!result) {
+            res.status(401).send('Login Failed');
+          } else {
+            delete result.dataValues.password;
+            const accessToken = generateAccessToken(result.dataValues)
+            const refreshToken = generateRefreshToken(result.dataValues);
+            req.session.save( () => {
+              //! session.userId에 uuid 활용
+              req.session.userId = result.dataValues.uuid;
+              res.cookie('refreshToken', refreshToken);
+              res.status(200).send({
+                data: {
+                  accessToken,
+                  username: result.dataValues.username
+                },
+                message: 'Login Successfully'
+              });
+            });
+          }
+      } else {
+        Users.findOne({
+          where: { uuid }
+        })
+        .then((result) => {
+          if(!result) {
+            res.status(401).send('Login Failed');
+          } else {
+            // TODO: accessToken과 refreshToken 생성
+            // TODO: req.session.userId 에 uuid 입력
+            // TODO: accessToken과 refreshToken 전송
+            // ! 엑세스 토큰을 뭘로 암호화 할건지? uuid로 암호화 한다 
+            delete result.dataValues.password;
+            const accessToken = generateAccessToken(result.dataValues.uuid);
+            const refreshToken = generateRefreshToken(result.dataValues.uuid);
+      
+            req.session.save( () => {
+              //! session.userId에 uuid 활용
+              req.session.userId = result.dataValues.uuid;
+              res.cookie('refreshToken', refreshToken);
+              res.status(200).send({
+                data: {
+                  accessToken,
+                  username: result.dataValues.username
+                },
+                message: 'Login Successfully'
+              });
+            });
+          }
       })
-      .then((result) => {
-        if(!result) {
-          res.status(401).send('Login Failed');
-        } else {
-          // TODO: accessToken과 refreshToken 생성
-          // TODO: req.session.userId 에 uuid 입력
-          // TODO: accessToken과 refreshToken 전송
-          // ! 엑세스 토큰을 뭘로 암호화 할건지? uuid로 암호화 한다 
-          delete result.dataValues.password;
-          const accessToken = generateAccessToken(result.dataValues.uuid);
-          const refreshToken = generateRefreshToken(result.dataValues.uuid);
-    
-          req.session.save( () => {
-            //! session.userId에 uuid 활용
-            req.session.userId = result.dataValues.uuid;
-            res.cookie('refreshToken', refreshToken);
-            res.status(200).send({
-              data: {
-                accessToken,
-                username: result.dataValues.username
-              },
-              message: 'Login Successfully'
-            });
-          });
-        }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(422).send('Insufficient Information');
-    });
-    }
-  },
-
+      .catch((err) => {
+        console.error(err);
+        res.cookie('refreshToken', refreshToken);
+        res.status(422).send('Insufficient Information');
+      });
+    }      
+  } else {
+    //*로그인이 된 경우
+    res.status(409).send('Already login');
+  }
+},
  //* 소셜로그인 할 때 프론트에서 authorizationCode를 전송해주면 accessToken을 깃허브로부터 받아서 전송해준다
   getTokenController: (req, res) => { 
     console.log("      🔍REQUEST CHECK🔍    ",req.body.authorizationCode);
@@ -160,20 +161,19 @@ module.exports = {
         accept: 'application/json',
       },
       data: {
-        client_id: '749cea90f0ee8535f1fa',
-        client_secret: 'dd32ef6bef3293b42cde199d6a968bf3f5375200',
+        client_id: `75d98169bb09be4ab543`,
+        client_secret: `4f2722a9eccf0a07b2b8670c5727e88b865ad9fe`,
         code: req.body.authorizationCode
       }
     }).then((result) => {
-      const accessToken = result.access_token;
-      const refreshToken = result.refresh_token;
+      const accessToken = result.data.access_token;
       console.log("        💡GITHUB DATA💡       ", result.data);
       console.log("ACCESS TOKEN: ", accessToken);
-      console.log("REFRESH TOKEN: ", refreshToken);
+      // res.cookie('accessToken', accessToken);
+      // res.status(200).send();
       res.status(200).send({
         data: {
-          accessToken,
-          refreshToken
+          accessToken
         }
       });
     }).catch((err) => {
@@ -183,6 +183,7 @@ module.exports = {
 
  //* 소셜로그인 한 유저가 회원인지 판별 컨트롤러(/logcheck)
   logCheckController: async(req, res) => {
+    console.log('확인용:',req.body);
     const { socialId } = req.body;
     await Users.findOne({
       where: { socialId }
@@ -190,7 +191,14 @@ module.exports = {
     .then((result) => {
       if(!result) {
         res.status(404).send('Not our client');
-      } res.status(202).send('recollect user');
+      } else {
+        res.status(202).send({
+          data: {
+            uuid: result.dataValues.uuid
+          },
+          message: 'recollect user'
+        })
+      } 
     })
     .catch((err) => {
       res.status(500).send(err);
@@ -199,7 +207,6 @@ module.exports = {
 
   //* 로그아웃 컨트롤러
   logoutController: (req, res) => {
-    //TODO: authorization 인지 Authorization인지 체크
     if(!req.headers.authorization) {
       res.status(403).send('Log Out Failed');
     } else {
